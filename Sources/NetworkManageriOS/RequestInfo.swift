c5f17905a560afbc2195b4675bcb1605b389fbcd
Glasovac <josip.glasovac@cinnamon.agency>
2022-07-19 15:36:36 +0200

    Request info public

--- Sources/NetworkManageriOS/RequestInfo.swift
+++ Sources/NetworkManageriOS/RequestInfo.swift
@@ -7,6 +7,14 @@ public struct RequestInfo {
     public var parameters: [String: Any]?
     public var isAuthorized: Bool = false
     public var header: [String: String]?
+
+    public init(url: String, method: HttpMethod = .GET, parameters: [String: Any]? = nil, isAuthorized: Bool = false, header: [String: String]?) {
+        self.url = url
+        self.method = method
+        self.parameters = parameters
+        self.isAuthorized = isAuthorized
+        self.header = header
+    }
 }
 
 public enum HttpMethod: String {
