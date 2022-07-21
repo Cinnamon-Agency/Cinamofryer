5d8efdd3fbf6f15d5941a17ca39e9ec70f391d40
Glasovac <josip.glasovac@cinnamon.agency>
2022-07-21 11:22:56 +0200

    RequestInfoUtils public

--- Sources/NetworkManageriOS/Utils/RequestInfoUtils.swift
+++ Sources/NetworkManageriOS/Utils/RequestInfoUtils.swift
@@ -4,7 +4,7 @@ public protocol RequestInfoUtils {
     func getParams<T: Codable>(_ value: T) throws -> [String: Any]
 }
 
-extension RequestInfoUtils {
+public extension RequestInfoUtils {
     func getParams<T: Codable>(_ value: T) throws -> [String: Any] {
         guard let parameters = try? value.asDictionary() else {
             throw NetworkError.failedToEncode
